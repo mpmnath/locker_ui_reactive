66f7d423cc713ed2daff369775fa413f7f8248a0
mpmnath <mpm.nath007@gmail.com>
2024-03-05 08:00:46 +0100

    Refactor PickupBoxWidget to use Orders instead of OrderBloc

--- lib/screens/widgets/pickupbox_widget.dart
+++ lib/screens/widgets/pickupbox_widget.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:tasks/bloc/order_bloc.dart';
+import 'package:tasks/core/orders.dart';
 import 'package:tasks/models/order_model.dart';
 import 'package:tasks/screens/pickupbox/pickupbox_empty.dart';
 import 'package:tasks/screens/pickupbox/pickupbox_with_order.dart';
 
 class PickupBoxWidget extends StatelessWidget {
   final int boxNumber;
-  final OrderBloc orderBloc;
+  final Orders orders;
 
-  const PickupBoxWidget(this.boxNumber, this.orderBloc, {super.key});
+  const PickupBoxWidget(this.boxNumber, this.orders, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<List<Order>>(
-      stream: orderBloc.orderStream as Stream<List<Order>>?,
+      stream: orders.orderStream,
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           List<Order> orders = snapshot.data!;
