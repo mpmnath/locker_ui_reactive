1827897524d72c8ab786119cabb14e547ce32d80
mpmnath <mpm.nath007@gmail.com>
2024-03-05 08:01:24 +0100

    Refactor pickup box terminal screen

--- lib/screens/pickupbox_terminal.dart
+++ lib/screens/pickupbox_terminal.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:tasks/bloc/order_bloc.dart';
+import 'package:tasks/core/orders.dart';
 import 'package:tasks/screens/terminal_screen/pending_orders_screen.dart';
 import 'package:tasks/screens/terminal_screen/pickup_screen.dart';
 import 'package:tasks/screens/widgets/pickupbox_widget.dart';
@@ -13,41 +13,64 @@ class PickupBoxTerminal extends StatefulWidget {
 }
 
 class _PickupBoxTerminalState extends State<PickupBoxTerminal> {
-  final OrderBloc _orderBloc = OrderBloc();
+  final Orders _orders = Orders();
 
   @override
   void dispose() {
-    _orderBloc.dispose();
+    _orders.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Pick Up Box Terminal'),
       ),
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        //height: MediaQuery.of(context).size.height / 2,
-        child: Row(
+      body: Container(
+        width: size.width,
+        height: size.height - 150,
+        decoration: BoxDecoration(
+          color: Colors.grey[100],
+          border: Border.all(
+            color: Colors.black,
+            width: 24.0,
+          ),
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        margin: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
+            GridView.builder(
+              shrinkWrap: true,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 8, // Assuming 4 columns for 8 pickup boxes
+                crossAxisSpacing: 16.0,
+                mainAxisSpacing: 16.0,
+              ),
+              itemCount: 8,
+              itemBuilder: (context, index) {
+                return PickupBoxWidget(index + 1, _orders);
+              },
+            ),
+            const SizedBox(height: 16.0),
             Expanded(
-              child: GridView.builder(
-                shrinkWrap: true,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4, // Assuming 4 columns for 8 pickup boxes
-                  crossAxisSpacing: 8.0,
-                  mainAxisSpacing: 8.0,
-                ),
-                itemCount: 8,
-                itemBuilder: (context, index) {
-                  return PickupBoxWidget(index + 1, _orderBloc);
-                },
+              child: Row(
+                children: [
+                  Expanded(
+                    child: PickupTerminalScreen(
+                        orders: _orders, orderStream: _orders.orderStream),
+                  ),
+                  const SizedBox(width: 16.0),
+                  Expanded(
+                    child: PendingOrdersTerminalScreen(orders: _orders),
+                  ),
+                ],
               ),
             ),
-            PickupTerminalScreen(orderBloc: _orderBloc),
-            PendingOrdersTerminalScreen(orderBloc: _orderBloc),
           ],
         ),
       ),
@@ -56,7 +79,7 @@ class _PickupBoxTerminalState extends State<PickupBoxTerminal> {
           // Simulate assigning an order to a random pickup box
           int orderId = Random().nextInt(500) +
               500; // Generating a random order ID between >=500 and <1000
-          _orderBloc.enqueueOrder(orderId);
+          _orders.enqueueOrder(orderId);
         },
         tooltip: 'Assign Random Order',
         child: const Icon(Icons.add),
