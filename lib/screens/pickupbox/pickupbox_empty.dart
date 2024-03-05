146989b6059974928c87feaf181ce848ec15d097
mpmnath <mpm.nath007@gmail.com>
2024-03-05 08:01:09 +0100

    Update border style and add box shadow to PickupBoxEmpty widget

--- lib/screens/pickupbox/pickupbox_empty.dart
+++ lib/screens/pickupbox/pickupbox_empty.dart
@@ -9,7 +9,29 @@ class PickupBoxEmpty extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        border: Border.all(),
+        //border: Border.all(),
+        border: Border.all(
+          color: Colors.black,
+          width: 2,
+        ), //Border.all
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.grey,
+            offset: Offset(
+              5.0,
+              5.0,
+            ), //Offset
+            blurRadius: 10.0,
+            spreadRadius: 2.0,
+          ), //BoxShadow
+          BoxShadow(
+            color: Colors.white,
+            offset: Offset(0.0, 0.0),
+            blurRadius: 0.0,
+            spreadRadius: 0.0,
+          ), //BoxShadow
+        ],
       ),
       child: Center(
         child: Text('Box $boxNumber\nEmpty'),
