10bdf2de73d329e038d7f834efcd29dc4c168697
mpmnath <mpm.nath007@gmail.com>
2024-03-05 08:00:59 +0100

    Refactor PendingOrdersTerminalScreen widget

--- lib/screens/terminal_screen/pending_orders_screen.dart
+++ lib/screens/terminal_screen/pending_orders_screen.dart
@@ -1,26 +1,26 @@
-
 import 'dart:collection';
-
 import 'package:flutter/material.dart';
-import 'package:tasks/bloc/order_bloc.dart';
+import 'package:tasks/core/orders.dart';
 
 class PendingOrdersTerminalScreen extends StatelessWidget {
-  final OrderBloc orderBloc;
-  const PendingOrdersTerminalScreen({super.key, required this.orderBloc});
+  final Orders orders;
+  const PendingOrdersTerminalScreen({super.key, required this.orders});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 300,
-      height: 400,
       decoration: BoxDecoration(
-        border: Border.all(),
+        border: Border.all(
+          color: Colors.black,
+          width: 1.0,
+        ),
+        borderRadius: BorderRadius.circular(16.0),
       ),
-      margin: const EdgeInsets.all(8.0),
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
           children: [
             const Text(
               'Pending Orders',
@@ -53,51 +53,53 @@ class PendingOrdersTerminalScreen extends StatelessWidget {
                 ),
               ),
             ),
-            StreamBuilder(
-              stream: orderBloc.orderQueueStream,
-              builder: (contex, snapshot) {
-                if (snapshot.hasData) {
-                  Queue<int> pendingOrders = snapshot.data!;
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: pendingOrders.length,
-                    itemBuilder: (context, index) {
-                      int orderId = pendingOrders.elementAt(index);
-                      return ListTile(
-                        visualDensity: const VisualDensity(vertical: -4),
-                        contentPadding: const EdgeInsets.all(0),
-                        leading: Text(
-                          '#$orderId',
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 12,
+            Expanded(
+              child: StreamBuilder(
+                stream: orders.orderQueueStream,
+                builder: (contex, snapshot) {
+                  if (snapshot.hasData) {
+                    Queue<int> pendingOrders = snapshot.data!;
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: pendingOrders.length,
+                      itemBuilder: (context, index) {
+                        int orderId = pendingOrders.elementAt(index);
+                        return ListTile(
+                          visualDensity: const VisualDensity(vertical: -4),
+                          contentPadding: const EdgeInsets.all(0),
+                          leading: Text(
+                            '#$orderId',
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12,
+                            ),
                           ),
-                        ),
-                        title: const Center(
-                          child: Text(
-                            'Pending',
+                          title: const Center(
+                            child: Text(
+                              'Pending',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ),
+                          trailing: const Text(
+                            '--:--',
                             style: TextStyle(
                               fontWeight: FontWeight.bold,
                               fontSize: 12,
                             ),
                           ),
-                        ),
-                        trailing: const Text(
-                          '--:--',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 12,
-                          ),
-                        ),
-                      );
-                    },
-                  );
-                } else {
-                  return const Center(
-                    child: Text('No Orders Assigned'),
-                  );
-                }
-              },
+                        );
+                      },
+                    );
+                  } else {
+                    return const Center(
+                      child: Text('No Orders Assigned'),
+                    );
+                  }
+                },
+              ),
             ),
           ],
         ),
